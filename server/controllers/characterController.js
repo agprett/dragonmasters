de9d6e1cd15a71b83413af0c97fae2924c1df1fe
Alec Prettyman <prettale4@gmail.com>
2023-09-08 15:17:35 -0600

    Updated character controller to use ORM

--- server/controllers/characterController.js
+++ server/controllers/characterController.js
@@ -13,28 +13,21 @@ const sequelize = new Sequelize(CONNECTION_STRING, {
 })
 
 const characterFunctions = {
-  getCharacters: (req, res) => {
+  getCharacters: async (req, res) => {
     if(req.session.user) {
       const {user_id} = req.session.user
       
-      sequelize.query(`
-        SELECT character_id, name, player, level, hit_points FROM characters
-        WHERE user_id = '${user_id}';
-      `)
-      .then(dbRes => {
-        let characters = dbRes[0].map(character => {
-          character.character_id = +character.character_id
-          character.level = +character.level
-          character.hit_points = +character.hit_points
-          
-          return character
-        })
+      let data = await Character.findAll({where: {user_id}, attributes: ['character_id', 'name', 'player', 'level', 'hit_points']})
 
-        res.status(200).send(characters)
+      let characters = data.map(character => {
+        character.character_id = +character.character_id
+        character.level = +character.level
+        character.hit_points = +character.hit_points
+        
+        return character
       })
-      .catch(err => {
-        res.sendStatus(500)
-      })
+
+      res.status(200).send(characters)
     } else {
       res.status(400).send('You must be signed in to view your characters!')
     }
@@ -43,7 +36,6 @@ const characterFunctions = {
   createCharacter: async (req, res) => {
     const {name, player, level, hit_points, race, char_class, armor_class} = req.body
 
-    
     if(req.session.user) {
       if(!name || !player || !hit_points) {
         res.status(400).send('You must provide all required info to create a character')
@@ -51,7 +43,6 @@ const characterFunctions = {
         const {user_id} = req.session.user
   
         let newChar = await Character.create({name, player, level, hit_points, race, char_class, armor_class, user_id}, {fields: ['name', 'player', 'hit_points', 'level', 'race', 'char_class', 'armor_class', 'user_id']})
-    
 
         res.status(200).send(`New character, ${newChar.name}, created!`)
       }
